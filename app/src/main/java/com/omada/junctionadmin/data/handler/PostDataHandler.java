e7637ccbb9095056cce5ab38abd3d48ec3deef20
anandanurag393 <61589296+anandanurag393@users.noreply.github.com>
2021-01-10 00:27:55 +0530

    Declared methods of PostDataHandler

--- app/src/main/java/com/omada/junctionadmin/data/handler/PostDataHandler.java
+++ app/src/main/java/com/omada/junctionadmin/data/handler/PostDataHandler.java
@@ -1,4 +1,48 @@
 package com.omada.junctionadmin.data.handler;
 
+import com.omada.junctionadmin.data.models.BaseModel;
+
 public class PostDataHandler {
+
+    public void  getOrganizationHighlights(String organizationID){
+
+    }
+
+    public void getInstituteHighlights(String instituteID){
+
+    }
+
+    public void getAllInstitutePosts(String  instituteID){
+
+    }
+
+    public void getAllOrganizationPosts(String organizationID){
+
+    }
+
+    public void updatePost(BaseModel baseModel){
+
+    }
+
+    public void createPost(BaseModel baseModel){
+
+    }
+
+    public void deletePost(String postID){
+
+    }
+
+    public void getEventRegistrations(String eventID){
+
+    }
+
+    public void updateOrganizationHighlights( String organizationID){
+
+    }
+
+    public void addToOrganizationShowcase(String showcaseID,String postID){
+
+    }
+
+
 }

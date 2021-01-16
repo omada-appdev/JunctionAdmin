3883a2f55250c44dbbca0e43e5e30df2de1417b0
Abdul Rahim Khan <abdulrahimkhan1999@gmail.com>
2021-01-16 17:49:44 +0300

    fixed a minor error

--- app/src/main/java/com/omada/junctionadmin/data/handler/OrganizationDataHandler.java
+++ app/src/main/java/com/omada/junctionadmin/data/handler/OrganizationDataHandler.java
@@ -1,10 +1,15 @@
 package com.omada.junctionadmin.data.handler;
 
 
+import androidx.lifecycle.LiveData;
+
+import com.omada.junctionadmin.data.models.external.OrganizationModel;
+import com.omada.junctionadmin.utils.taskhandler.LiveEvent;
+
 //user is organization
 public class OrganizationDataHandler {
-    public void getOrganizationDetails(String organizationID) {
-
+    public LiveData<LiveEvent<OrganizationModel>> getOrganizationDetails(String organizationID) {
+        return null;
     }
     public void getInstituteOrganizations(String organizationID) {
 
